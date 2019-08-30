85abba51e1b90fda5694cbd3bffd35c96b5c653a
NZXT-H440\diXx <gamtokarev@gmail.com>
2019-08-31 00:07:58 +0300

    platform define init commit

--- XMake/src/platform.h
+++ XMake/src/platform.h
@@ -1,8 +1,18 @@
 #pragma once
 
+#if defined (_WIN64)
 
-#ifdef _WIN64
+    #define PLATFORM_WINDOWS
 
+#elif defined (__linux__)
+
+    #define PLATFORM_LINUX
+
+#endif
+
+#ifdef PLATFORM_WINDOWS
+
+    #define WIN32_LEAN_AND_MEAN
     #define _CRT_SECURE_NO_WARNINGS
     #define OS "WINDOWS"
     #define CONFIG "X64"
@@ -12,6 +22,14 @@
 #include <fstream>
 #include <cstdio>
 
-#elif __linux__
+#elif defined PLATFORM_LINUX
+    
+    #define OS "LINUX"
+    #define CONFIG "X64"
+    #define MAXLENGHT 1000
+
+#include <unistd.h>
 
 #endif
+
+
